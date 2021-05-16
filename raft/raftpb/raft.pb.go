a858a4d32e15365fb88dda6e5e43a35ff96023df
Yi-Jiang Yang <mg1933080@smail.nju.edu.cn>
2021-05-16 14:57:04 +0800

    give to new number to MsgDrSync and MsgDrSyncResp

--- raft/raftpb/raft.pb.go
+++ raft/raftpb/raft.pb.go
@@ -104,8 +104,8 @@ const (
 	MsgPreVoteResp    MessageType = 18
 	MsgPerMode        MessageType = 40
 	MsgPerModeResp    MessageType = 41
-	MsgDRSync              MessageType = 46
-	MsgDRSyncResp          MessageType = 47
+	MsgDRSync         MessageType = 42
+	MsgDRSyncResp     MessageType = 43
 )
 
 var MessageType_name = map[int32]string{
@@ -130,34 +130,34 @@ var MessageType_name = map[int32]string{
 	18: "MsgPreVoteResp",
 	40: "MsgPerMode",
 	41: "MsgPerModeResp",
-	46: "MsgDRSync",
-	47: "MsgDRSyncResp",
+	42: "MsgDRSync",
+	43: "MsgDRSyncResp",
 }
 
 var MessageType_value = map[string]int32{
-	"MsgHup":                 0,
-	"MsgBeat":                1,
-	"MsgProp":                2,
-	"MsgApp":                 3,
-	"MsgAppResp":             4,
-	"MsgVote":                5,
-	"MsgVoteResp":            6,
-	"MsgSnap":                7,
-	"MsgHeartbeat":           8,
-	"MsgHeartbeatResp":       9,
-	"MsgUnreachable":         10,
-	"MsgSnapStatus":          11,
-	"MsgCheckQuorum":         12,
-	"MsgTransferLeader":      13,
-	"MsgTimeoutNow":          14,
-	"MsgReadIndex":           15,
-	"MsgReadIndexResp":       16,
-	"MsgPreVote":             17,
-	"MsgPreVoteResp":         18,
+	"MsgHup":            0,
+	"MsgBeat":           1,
+	"MsgProp":           2,
+	"MsgApp":            3,
+	"MsgAppResp":        4,
+	"MsgVote":           5,
+	"MsgVoteResp":       6,
+	"MsgSnap":           7,
+	"MsgHeartbeat":      8,
+	"MsgHeartbeatResp":  9,
+	"MsgUnreachable":    10,
+	"MsgSnapStatus":     11,
+	"MsgCheckQuorum":    12,
+	"MsgTransferLeader": 13,
+	"MsgTimeoutNow":     14,
+	"MsgReadIndex":      15,
+	"MsgReadIndexResp":  16,
+	"MsgPreVote":        17,
+	"MsgPreVoteResp":    18,
 	"MsgPerMode":        40,
 	"MsgPerModeResp":    41,
-	"MsgDRSync":              46,
-	"MsgDRSyncResp":          47,
+	"MsgDRSync":         42,
+	"MsgDRSyncResp":     43,
 }
 
 func (x MessageType) Enum() *MessageType {
