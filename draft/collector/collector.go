6e44b2f6b799696164f7e466f963177da637dea4
Yi-Jiang Yang <mg1933080@smail.nju.edu.cn>
2021-06-11 16:53:14 +0800

    + fix a bug in addEntries when facing a resending scenario + redefine the meaning of regularization + relieve Collector interface from Briefer interface

--- draft/collector/collector.go
+++ draft/collector/collector.go
@@ -32,16 +32,17 @@ type Collector interface {
 	FetchEntries(term uint64) (bool, []raftpb.Entry, uint64, uint64)
 
 	EntryFetcher
-	Briefer
 	Refresher
 }
 
-type subCollector struct {
+type nodeCollector struct {
 	Collector
-	brief []*BriefSegment
-	info  uint64
-	prev  *subCollector
-	next  *subCollector
+
+	startIndex uint64
+
+	info uint64
+	prev *nodeCollector
+	next *nodeCollector
 }
 
 //LinkedListCollector is an implementation of Collector. It allows
@@ -54,8 +55,8 @@ type subCollector struct {
 // If you want to relieve the burden from regularization, make sure defaultRegOpt is true - the collector
 // will not allow the temporary overlapping during AddEntries.
 type LinkedListCollector struct {
-	head *subCollector
-	tail *subCollector
+	head *nodeCollector
+	tail *nodeCollector
 
 	regularized   bool
 	defaultRegOpt bool
@@ -63,12 +64,12 @@ type LinkedListCollector struct {
 	cg func() Collector
 }
 
-func (c *LinkedListCollector) SubCECFactory() func() Collector {
+func (c *LinkedListCollector) NodeFactory() func() Collector {
 	return c.cg
 }
 
-func (c *LinkedListCollector) SetSubCECFactory(subCECFactory func() Collector) {
-	c.cg = subCECFactory
+func (c *LinkedListCollector) SetNodeFactory(nodeFactory func() Collector) {
+	c.cg = nodeFactory
 }
 
 func NewLinkedListCollector(defaultRegOpt bool, cg func() Collector) *LinkedListCollector {
@@ -76,11 +77,11 @@ func NewLinkedListCollector(defaultRegOpt bool, cg func() Collector) *LinkedList
 }
 
 func (c *LinkedListCollector) AddEntries(entries []raftpb.Entry, logTerm uint64, logIndex uint64) bool {
-	if c.regularized {
-		c.regularizedAddEntries(entries, logTerm, logIndex)
-	} else {
-		c.addEntries(entries, logTerm, logIndex)
+	if len(entries) == 0 {
+		return false
 	}
+
+	c.addEntries(c.regularized, entries, logTerm, logIndex)
 
 	return true
 }
@@ -89,11 +90,12 @@ func (c *LinkedListCollector) FetchEntries(term uint64) (bool, []raftpb.Entry, u
 	if c.IsNotInitialized() {
 		return false, nil, 0, 0
 	}
-
-	c.regularize()
-
-	if sub := c.locateEntries(term); sub != nil {
-		return sub.FetchEntries(term)
+	needle := c.head
+	for needle != nil {
+		if ok, ent, lt, li := needle.FetchEntries(term); ok {
+			return true, ent, lt, li
+		}
+		needle = needle.next
 	}
 
 	return false, nil, 0, 0
@@ -103,8 +105,6 @@ func (c *LinkedListCollector) FetchEntriesWithStartIndex(index uint64) (bool, []
 	if c.IsNotInitialized() {
 		return false, nil, 0, 0
 	}
-
-	c.regularize()
 
 	_, e, t, i := c.head.FetchEntriesWithStartIndex(index)
 
@@ -116,40 +116,15 @@ func (c *LinkedListCollector) FetchAllEntries() (bool, []raftpb.Entry, uint64, u
 		return false, nil, 0, 0
 	}
 
-	c.regularize()
-
 	_, e, t, i := c.head.FetchAllEntries()
 
 	return c.head.next == nil, e, t, i
 }
 
 func (c *LinkedListCollector) Refresh() {
-	if c.head == nil {
-		return
-	}
-	c.head.brief = nil
-	c.head.Refresh()
-	if c.head.next != nil {
-		c.head.next.prev = nil
-		c.head.next = nil
-	}
+	c.head = nil
+	c.tail = nil
 	c.regularized = c.defaultRegOpt
-}
-
-func (c *LinkedListCollector) Briefing() []*BriefSegment {
-	if c.head == nil {
-		return nil
-	}
-
-	if c.head.brief == nil {
-		var ok bool
-		c.head.brief = c.head.Briefing()
-		if !ok {
-			return nil
-		}
-	}
-
-	return c.briefing()
 }
 
 func (c *LinkedListCollector) IsNotInitialized() bool {
@@ -169,184 +144,105 @@ func (c *LinkedListCollector) Regularized() bool {
 }
 
 func (c *LinkedListCollector) SetRegularized(regularized bool) {
-	if !c.regularized && regularized {
-		c.regularize()
-	}
-
 	c.regularized = regularized
 }
 
-func (c *LinkedListCollector) briefing() []*BriefSegment {
-	var result []*BriefSegment
-
-	needle := c.head
-	for needle != nil {
-		if needle.brief == nil {
-			needle.brief = needle.Briefing()
-		}
-		result = append(result, needle.brief...)
-		needle = needle.next
-	}
-
-	return result
-}
-
-func (c *LinkedListCollector) locateEntries(term uint64) Collector {
-	needle := c.head
-	for needle != nil {
-		if needle.brief == nil {
-			needle.brief = needle.Briefing()
-		}
-
-		b := needle.brief
-
-		if b[0].Term > term {
-			return nil
-		} else if term <= b[len(b)-1].Term {
-			var found = false
-			for _, s := range b {
-				if s.Term == term {
-					found = true
-					break
-				}
-			}
-
-			if found {
-				return needle
-			}
-		}
-
-		needle = needle.next
-	}
-
-	return nil
-}
-
-func (c *LinkedListCollector) regularizedAddEntries(entries []raftpb.Entry, logTerm uint64, logIndex uint64) {
+func (c *LinkedListCollector) addEntries(regularize bool, entries []raftpb.Entry, logTerm uint64, logIndex uint64) *nodeCollector {
 	if c.head == nil {
-		c.addSubAndMoveToTail().AddEntries(entries, logTerm, logIndex)
-		return
-	}
-
-	needle := c.head
-
-	for needle != nil {
-		if ok := needle.AddEntries(entries, logTerm, logIndex); ok {
-			next := needle.next
-			if next != nil {
-				next.prev = nil
-			}
-			needle.next = nil
-			return
-		}
-		needle = needle.next
-	}
-
-	c.addSubAndMoveToTail().AddEntries(entries, logTerm, logIndex)
-}
-
-func (c *LinkedListCollector) addEntries(entries []raftpb.Entry, logTerm uint64, logIndex uint64) {
-	if c.head == nil {
-		c.addSubAndMoveToTail()
-	}
-
-	c.tail.brief = nil
-
-	if ok := c.tail.AddEntries(entries, logTerm, logIndex); !ok {
-		c.addSubAndMoveToTail().AddEntries(entries, logTerm, logIndex)
-	}
-}
-
-//todo: verify and make it compatible to info fields
-func (c *LinkedListCollector) regularize() {
-	if c.regularized {
-		return
-	}
-
-	if c.head == nil || c.head.next == nil {
-		c.regularized = true
-		return
-	}
-
-	for {
-		needle := c.head
-		regular := true
-
-		for needle != nil {
-			if needle.brief == nil {
-				break
-			}
-			// clear out buffered brief
-			needle.brief = nil
-
-			if needle.IsNotInitialized() {
-				needle = c.tryRmvSubAndMoveToNext(needle)
-				continue
-			}
-
-			next := needle.next
-
-			for next != nil {
-				ok, ent, lt, li := next.FetchAllEntries()
-
-				if ok && !needle.AddEntries(ent, lt, li) {
-					regular = false
-					break
-				}
-
-				next = c.tryRmvSubAndMoveToNext(next)
-			}
-
-			needle = needle.next
-		}
-
-		if regular {
-			break
-		}
-	}
-
-	c.regularized = true
-}
-
-func (c *LinkedListCollector) addSubAndMoveToTail() *subCollector {
-	if c.head == nil {
-		c.head = &subCollector{Collector: c.cg()}
+		c.head = c.newNodeCollector(entries, logTerm, logIndex)
 		c.tail = c.head
-	} else {
-		c.tail.next = &subCollector{Collector: c.cg(), prev: c.tail}
-		c.tail = c.tail.next
-	}
-
-	return c.tail
-}
-
-func (c *LinkedListCollector) tryRmvSubAndMoveToNext(sc *subCollector) *subCollector {
-	if c.head == sc {
-		if sc.next == nil {
-			return nil
-		}
-		c.head = sc.next
-		sc.next = nil
-		c.head.prev = nil
-
 		return c.head
 	}
 
-	if c.tail == sc {
-		c.tail = sc.prev
-		sc.prev = nil
-		c.tail.next = nil
+	needle := c.head
 
-		return nil
+	for needle != nil {
+		if needle.startIndex > logIndex+1 {
+			// underflow occurs, reverse the order of the node
+			node := c.newNodeCollector(entries, logTerm, logIndex)
+			c.insertBefore(node, needle)
+
+			if regularize {
+				c.regularizeFrom(node)
+			} else {
+				c.deleteAfter(node)
+			}
+
+			return node
+		}
+
+		if ok := needle.AddEntries(entries, logTerm, logIndex); ok {
+			c.deleteAfter(needle)
+			return needle
+		}
+
+		needle = needle.next
 	}
 
-	next := sc.next
-	sc.next = nil
-	prev := sc.prev
-	sc.prev = nil
+	node := c.newNodeCollector(entries, logTerm, logIndex)
+	c.insertAfter(c.tail, node)
+	return node
+}
 
-	prev.next = next
-	next.prev = prev
+func (c *LinkedListCollector) newNodeCollector(entries []raftpb.Entry, logTerm, logIndex uint64) *nodeCollector {
+	node := &nodeCollector{
+		Collector:  c.cg(),
+		startIndex: logIndex + 1,
+	}
 
-	return next
+	node.AddEntries(entries, logTerm, logIndex)
+	return node
+}
+
+func (c *LinkedListCollector) insertAfter(node *nodeCollector, after *nodeCollector) {
+	node.prev = after
+	node.next = after.next
+	after.next = node
+
+	if node.next == nil {
+		c.tail = node
+	} else {
+		node.next.prev = node
+	}
+}
+
+func (c *LinkedListCollector) insertBefore(node *nodeCollector, before *nodeCollector) {
+	node.next = before
+	node.prev = before.prev
+	before.prev = node
+
+	if node.prev == nil {
+		c.head = node
+	} else {
+		node.prev.next = node
+	}
+}
+
+func (c *LinkedListCollector) deleteAfter(node *nodeCollector) {
+	if node.next != nil {
+		node.next.prev = nil
+	}
+	node.next = nil
+	c.tail = node
+}
+
+func (c *LinkedListCollector) deleteBefore(node *nodeCollector) {
+	if node.prev != nil {
+		node.prev.next = nil
+	}
+	node.prev = nil
+	c.head = node
+}
+
+func (c *LinkedListCollector) regularizeFrom(node *nodeCollector) {
+	needle := node.next
+	for needle != nil {
+		ok, ent, lt, li := needle.FetchAllEntries()
+		if ok && node.AddEntries(ent, lt, li) {
+			needle = needle.next
+		} else {
+			break
+		}
+	}
+	c.deleteAfter(node)
 }
